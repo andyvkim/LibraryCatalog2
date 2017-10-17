cf89def8765fdbc83420056701060258bbacfbd2
andyvkim <andyvkim@gmail.com>
2017-10-16 20:26:49 -0400

    Add files via upload

---
+++ LibraryCatalog2/LibraryData/Models/Patron.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephoneNumber { get; set; }
+
+        //public virtual LibraryCard LibraryCard { get; set; }//entity framework will make patron object to a libarycard object
+
+    }
+}
